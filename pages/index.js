7376694c8dcf9c3879d03801ef13e2128c5d1d2e
apicazorla <76844926+apicazorla@users.noreply.github.com>
2022-08-07 00:42:37 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+// @generated: @expo/next-adapter@2.1.5
+import { StyleSheet,Text, Linking} from 'react-native'
+
+import Page from '../components/page/Page'
+import { H1 } from '../components/webElements'
+import VideoPlayer from '../components/VideoPlayer'
+
+import { config } from '../config/config'
+
+export default function StartPage () {
+  return (
+    <Page>
+      <H1 style={styles.h1}>Escutoides de miel</H1>
+      <Text style={styles.text}>Es un producto de {config.appName}</Text>
+      <Text style={styles.text}>{config.appTagline}</Text>
+      <VideoPlayer
+        videoUrl='https://fisicayquimica2.s3.eu-west-3.amazonaws.com/escutoides.mp4'
+      />
+      <Text style={styles.text}>Más información en</Text>
+      <Text style={{color: 'blue'}}
+      onPress={() => Linking.openURL('http://apicazorla.com/post/getting-started')}>
+  Apicazorla
+</Text>
+    </Page>
+
+  )
+}
+
+
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 16,
+    marginTop: 8,
+    marginBottom: 8
+  },
+  h1: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 12,
+    marginBottom: 12
+  }
+  })
